ab0ef250f0bb5a2e91ca9cab808f5c65a6351e7f
Mahdi Moradi <mahdi.moradi.w@gmail.com>
2021-10-13 16:09:55 +0330

    make models (story, picture, comment) in bulletin app

--- bulletin/admin.py
+++ bulletin/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at', 'updated_at']
+
+
+@admin.register(Picture)
+class PictureAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at', 'updated_at']
--- bulletin/models.py
+++ bulletin/models.py
@@ -1,3 +1,63 @@
+from uuid import uuid4
 from django.db import models
+from django.utils.timezone import now
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericRelation
 
-# Create your models here.
+
+def picture_location(instance, filename, **kwargs):
+    return f'pictures/{instance.id}/image-{uuid4()}.{filename.split("/")[-1]}'
+
+
+def story_location(instance, filename, **kwargs):
+    return f'stories/{instance.id}/story-{uuid4()}.{filename.split("/")[-1]}'
+
+
+class CommonInfo(models.Model):
+    created_at = models.DateTimeField(_('created_at'), default=now)
+    updated_at = models.DateTimeField(_('updated_at'), auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Comment(CommonInfo):
+    PENDING = 'PE'
+    ACCEPTED = 'AC'
+    REJECTED = 'RE'
+    PUB_STATUS = [
+        (PENDING, 'Pending'),
+        (ACCEPTED, 'Accepted'),
+        (REJECTED, 'Rejected')
+    ]
+    author = models.CharField(max_length=255, null=False, blank=False)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=now, null=True)
+    published_status = models.CharField(max_length=2, choices=PUB_STATUS, null=False, default=PENDING)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+    def __str__(self):
+        return self.content
+
+    class Meta:
+        verbose_name_plural = "comments"
+
+
+class Picture(CommonInfo):
+    image = models.ImageField(_('main picture'), upload_to=picture_location, null=False)
+    comments = GenericRelation(Comment)
+
+
+class Story(CommonInfo):
+    file = models.FileField(_('story file'), upload_to=story_location, null=False)
+    comments = GenericRelation(Comment)
+
+    class Meta:
+        verbose_name_plural = "stories"
+
+
