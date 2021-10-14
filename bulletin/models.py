e3eb242faf596a53714f85fa53870fb1560d934f
Mahdi Moradi <mahdi.moradi.w@gmail.com>
2021-10-15 02:16:56 +0330

    custom faker and remove all migrations(duo to massive changes to models)

--- bulletin/models.py
+++ bulletin/models.py
@@ -6,52 +6,48 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericRelation
 
+PENDING = 'PE'
+ACCEPTED = 'AC'
+REJECTED = 'RE'
+PUB_STATUS = [
+    (PENDING, 'Pending'),
+    (ACCEPTED, 'Accepted'),
+    (REJECTED, 'Rejected')
+]
+
 
 def picture_location(instance, filename, **kwargs):
     return f'pictures/image-{uuid4()}_{filename.split("/")[-1]}'
 
 
-def story_location(instance, filename, **kwargs):
-    return f'stories/story-{uuid4()}_{filename.split("/")[-1]}'
-
-
 class CommonInfo(models.Model):
     created_at = models.DateTimeField(_('created_at'), default=now)
     updated_at = models.DateTimeField(_('updated_at'), auto_now=True)
+    posted_at = models.DateTimeField(default=None, null=True)
+    published_status = models.CharField(max_length=2, choices=PUB_STATUS, null=False, default=PENDING)
 
     class Meta:
         abstract = True
 
 
 class Comment(CommonInfo):
-    PENDING = 'PE'
-    ACCEPTED = 'AC'
-    REJECTED = 'RE'
-    PUB_STATUS = [
-        (PENDING, 'Pending'),
-        (ACCEPTED, 'Accepted'),
-        (REJECTED, 'Rejected')
-    ]
     author = models.CharField(max_length=255, null=False, blank=False)
-    content = models.TextField()
-    posted_at = models.DateTimeField(default=now, null=True)
-    published_status = models.CharField(max_length=2, choices=PUB_STATUS, null=False, default=PENDING)
-
+    text = models.TextField()
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
 
     def __str__(self):
-        return self.content
+        return self.text
 
     class Meta:
         verbose_name_plural = "comments"
 
 
 class Picture(CommonInfo):
-    image = models.ImageField(_('main picture'), upload_to=picture_location, null=False)
-    title = models.CharField(max_length=500, null=False, blank=False)
     uploader = models.CharField(max_length=255, null=False, blank=False)
+    title = models.CharField(max_length=500, null=False, blank=False)
+    image = models.ImageField(_('main picture'), upload_to=picture_location, null=False)
     comments = GenericRelation(Comment, content_type_field='content_type', object_id_field='object_id')
 
     def __str__(self):
@@ -59,9 +55,9 @@ class Picture(CommonInfo):
 
 
 class Story(CommonInfo):
-    text = models.TextField(null=False, blank=False)
-    title = models.CharField(max_length=500, null=False, blank=False)
     author = models.CharField(max_length=255, null=False, blank=False)
+    title = models.CharField(max_length=500, null=False, blank=False)
+    text = models.TextField(null=False, blank=False)
     comments = GenericRelation(Comment, content_type_field='content_type', object_id_field='object_id')
 
     def __str__(self):
---
+++ custom_faker.py
@@ -0,0 +1,106 @@
+import os
+import django
+import datetime
+from faker import Faker
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hoopoe.settings")
+django.setup()
+
+# placement of these imports are important ,don't change it
+from bulletin import models
+import requests
+from random import randint
+
+random_words = ['driver', 'philosophy', 'literature', 'inspection', 'shopping', 'topic',
+                'cigarette', 'employee', 'music', 'software', 'advice', 'movie', 'bonus',
+                'promotion', 'thanks', 'negotiation', 'nature', 'strategy', 'art', 'guest',
+                'apartment', 'indication', 'awareness', 'computer', 'poet', 'city', 'psychology',
+                'feedback', 'tension', 'thought', 'conclusion', 'cousin', 'recommendation',
+                'county', 'intention', 'quantity', 'maintenance', 'photo', 'shirt', 'outcome',
+                'replacement', 'gene', 'law', 'disaster', 'writer', 'discussion', 'effort',
+                'cabinet', 'performance', 'ambition', 'camera', 'importance', 'death', 'depth',
+                'obligation', 'independence', 'reflection', 'bird', 'possibility', 'hair',
+                'statement', 'sister', 'bread', 'agreement', 'expression', 'area', 'priority',
+                'error', 'disaster', 'percentage', 'storage', 'establishment', 'charity', 'diamond',
+                'tradition', 'equipment', 'sympathy', 'consequence', 'history', 'politics', 'celebration',
+                'hearing', 'world', 'physics', 'nation', 'goal', 'responsibility', 'protection', 'library',
+                'drama', 'contract', 'activity', 'tension', 'event', 'initiative', 'manufacturer',
+                'advertising', 'potato', 'story', 'effort']
+
+
+def rand_word():
+    return random_words[randint(0, len(random_words))]
+
+
+# we also can use pytz package
+class UTC0330(datetime.tzinfo):
+    # can be configured here
+    _offset = datetime.timedelta(seconds=12600)
+    _dst = datetime.timedelta(0)
+    _name = "+0330"
+
+    def utcoffset(self, dt):
+        return self.__class__._offset
+
+    def dst(self, dt):
+        return self.__class__._dst
+
+    def tzname(self, dt):
+        return self.__class__._name
+
+
+faker = Faker()
+Faker.seed(0)
+deep_ai_key = os.getenv("DEEP_AI")
+story_num = 20
+picture_num = 20
+
+for i in range(story_num):
+    r = requests.post(
+        "https://api.deepai.org/api/text-generator",
+        data={
+            'text': rand_word(),
+        },
+        headers={'api-key': deep_ai_key}
+    )
+    text = r.json()['output']
+
+    story = models.Story(
+        published_status=models.ACCEPTED,
+        author=faker.name(),
+        title=f'{rand_word()} is such a amazing {rand_word()}.',
+        text=text,
+    )
+    story.save()
+    for j in range(0, randint(1, 10)):
+        comment = models.Comment(
+            published_status=models.ACCEPTED,
+            author=faker.name(),
+            text=faker.paragraph(nb_sentence=7),
+            content_object=story,
+        )
+        comment.save()
+
+for i in range(picture_num):
+    response = requests.get("https://picsum.photos/400/300")
+    file_name = response.headers['Content-Disposition'].split(';')[1].strip().split("\"")[1]
+    local_file = f'media/tmp/{file_name}'
+    with open(local_file, 'wb') as file:
+        file.write(response.content)
+        picture = models.Picture(
+            published_status=models.ACCEPTED,
+            uploader=faker.name(),
+            title=f'{rand_word()} is such a amazing {rand_word()}.',
+            image=file,
+        )
+        picture.save()
+        for j in range(0, randint(1, 10)):
+            comment = models.Comment(
+                published_status=models.ACCEPTED,
+                author=faker.name(),
+                text=faker.paragraph(nb_sentence=7),
+                content_object=picture,
+            )
+            comment.save()
+
+
